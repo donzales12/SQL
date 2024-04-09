09758fe8ef0a693b221409f349a1ff4f0ef9c7d9
donzales12 <wadadon_salad@icloud.com>
2024-04-10 08:18:53 +0900

    added comment in 09

--- SQLPUZZLE/Answer/パズル09.sql
+++ SQLPUZZLE/Answer/パズル09.sql
@@ -1,3 +1,25 @@
+CREATE VIEW Firstseat (seat)
+AS SELECT (seat + 1)
+     FROM Restaurant
+    WHERE (seat + 1) NOT IN (SELECT seat FROM Restaurant)
+      AND (seat + 1) < 1001;
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+CREATE VIEW Lastseat (seat)
+AS SELECT (seat - 1)
+     FROM Restaurant
+    WHERE (seat - 1) NOT IN (SELECT seat FROM Restaurant)
+      AND (seat - 1) > 0;
+
+SELECT F1.seat AS start, L1.seat AS finish,
+       ((L1.seat - F1.seat) + 1) AS available
+  FROM Firstseat AS F1, Lastseat AS L1
+ WHERE L1.seat = (SELECT MIN(L2.seat)
+                    FROM Lastseat AS L2
+                   WHERE F1.seat <= L2.seat);
+
+-- 結合版
 SELECT (R1.seat + 1) AS start,
        (MIN(R2.seat) - 1) AS finish
   FROM Restaurant AS R1
